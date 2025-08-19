39d731b91660df99c6e4ead29fcee5865536975a
sdpb21 <jytprf@gmail.com>
2025-08-19 11:39:49 -0300

    Source code from youtube example builded successfully

--- ESP32/servo_test/main/pwm_youtube_example.c
+++ ESP32/servo_test/main/pwm_youtube_example.c
@@ -1,104 +1,161 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "esp_log.h"
-#include "led_strip.h"
-#include "sdkconfig.h"
+#include "freertos/timers.h"
+#include "driver/ledc.h"
 
-static const char *TAG = "example";
+#define led1 2
+uint8_t led_level = 0;
+static const char *tag = "Main";
+TimerHandle_t xTimers;
+int interval = 50;
+int timerId = 1;
+uint32_t dutyR = 0;
+uint32_t dutyG = 300;
+uint32_t dutyB = 600;
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+esp_err_t init_led(void);
+esp_err_t blink_led(void);
+esp_err_t set_timer(void);
+esp_err_t set_pwm(void);
+esp_err_t set_pwm_duty(void);
 
-static uint8_t s_led_state = 0;
+void vTimerCallback(TimerHandle_t pxTimer){
+    
+    dutyR += 10;
+    if( dutyR > 1023 )
+        dutyR = 0;
+    
+    dutyG += 10;
+    if( dutyG > 1023 )
+        dutyG = 0;
+    
+    dutyB += 10;
+    if( dutyB > 1023 )
+        dutyB = 0;
+    
+    blink_led();
+    set_pwm_duty();
 
-#ifdef CONFIG_BLINK_LED_STRIP
+}
 
-static led_strip_handle_t led_strip;
+void app_main(void){
 
-static void blink_led(void)
-{
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
+    init_led();
+    set_pwm();
+    set_timer();
+
+}
+
+esp_err_t init_led(void){
+    
+    gpio_reset_pin(led1);
+    gpio_set_direction(led1, GPIO_MODE_OUTPUT);
+
+    return ESP_OK;
+
+}
+
+esp_err_t blink_led(void){
+    
+    led_level = !led_level;
+    gpio_set_level(led1, led_level);
+
+    return ESP_OK;
+
+}
+
+esp_err_t set_timer(void){
+    
+    xTimers = xTimerCreate("Timer",                     // Just a text name, not used by the kernel.
+                           ( pdMS_TO_TICKS(interval)),  // The timer period in ticks.
+                           pdTRUE,                      // The timers will auto-reload themselves when they expire.
+                           (void *)timerId,             // Assign each timer a unique id.
+                           vTimerCallback               // Each timer calls the same callback when it expires.
+    );
+
+    if (xTimers == NULL)
+    {
+        // The timer was not created.
     }
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
-    };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
-}
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#else
-#error "unsupported LED type"
-#endif
-
-void app_main(void)
-{
-
-    /* Configure the peripheral according to the LED type */
-    configure_led();
-
-    while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    else
+    {
+        // Start the timer.  No block time is specified, and even if one was
+        // it would be ignored because the scheduler has not yet been
+        // started.
+        if (xTimerStart(xTimers, 0) != pdPASS)  // this line starts the timer
+        {
+            // The timer could not be set into the Active state.
+        }
     }
+
+    return ESP_OK;
+
+}
+
+esp_err_t set_pwm(void){
+    
+    // channel configure structures declaration
+    ledc_channel_config_t channelConfigR = {0};
+    ledc_channel_config_t channelConfigG = {0};
+    ledc_channel_config_t channelConfigB = {0};
+
+    // channel R structure initialization
+    channelConfigR.gpio_num = 33;
+    channelConfigR.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigR.channel = LEDC_CHANNEL_0;
+    channelConfigR.intr_type = LEDC_INTR_DISABLE;
+    channelConfigR.timer_sel = LEDC_TIMER_0;
+    channelConfigR.duty = 0;
+
+    // channel G structure initialization
+    channelConfigG.gpio_num = 25;
+    channelConfigG.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigG.channel = LEDC_CHANNEL_1;
+    channelConfigG.intr_type = LEDC_INTR_DISABLE;
+    channelConfigG.timer_sel = LEDC_TIMER_0;
+    channelConfigG.duty = 0;
+
+    // channel B structure initialization
+    channelConfigB.gpio_num = 26;
+    channelConfigB.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigB.channel = LEDC_CHANNEL_2;
+    channelConfigB.intr_type = LEDC_INTR_DISABLE;
+    channelConfigB.timer_sel = LEDC_TIMER_0;
+    channelConfigB.duty = 0;
+
+    // PWM channels configurations
+    ledc_channel_config( &channelConfigR );
+    ledc_channel_config( &channelConfigG );
+    ledc_channel_config( &channelConfigB );
+
+    // timer config structure declaration
+    ledc_timer_config_t timerConfig = {0};
+
+    // timerConfig structure fields initialization
+    timerConfig.speed_mode = LEDC_HIGH_SPEED_MODE;
+    timerConfig.duty_resolution = LEDC_TIMER_10_BIT;
+    timerConfig.timer_num = LEDC_TIMER_0;
+    timerConfig.freq_hz = 20000;    // 20kHz
+
+    // PWM timer configuration
+    ledc_timer_config( &timerConfig );
+
+    return ESP_OK;
+}
+
+esp_err_t set_pwm_duty(void){
+
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, dutyR);
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, dutyG);
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, dutyB);
+
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2);
+
+    return ESP_OK;
+
 }
