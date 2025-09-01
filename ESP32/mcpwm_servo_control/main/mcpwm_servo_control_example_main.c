1efe7a65e687e7a6baeba3382173f3ca9b425aa7
sdpb21 <jytprf@gmail.com>
2025-09-01 17:06:50 -0300

    GPIO changed

--- ESP32/mcpwm_servo_control/main/mcpwm_servo_control_example_main.c
+++ ESP32/mcpwm_servo_control/main/mcpwm_servo_control_example_main.c
@@ -17,7 +17,7 @@ static const char *TAG = "example";
 #define SERVO_MIN_DEGREE        -90   // Minimum angle
 #define SERVO_MAX_DEGREE        90    // Maximum angle
 
-#define SERVO_PULSE_GPIO             0        // GPIO connects to the PWM signal line
+#define SERVO_PULSE_GPIO             26       // GPIO connects to the PWM signal line
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
