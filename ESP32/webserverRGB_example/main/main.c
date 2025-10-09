10a93c39d1cd36d2fb9553b243bd7e3d7b86a26c
sdpb21 <jytprf@gmail.com>
2025-10-09 12:58:23 -0300

    Unrolling the example_connect function, day 2

--- ESP32/webserverRGB_example/main/main.c
+++ ESP32/webserverRGB_example/main/main.c
@@ -263,7 +263,99 @@ void app_main(void)
         
         Read "Establishing Wi-Fi or Ethernet Connection" section in
         examples/protocols/README.md for more information about this function. */
-    ESP_ERROR_CHECK(example_connect());
+    //ESP_ERROR_CHECK(example_connect());
+    //if (example_wifi_connect() != ESP_OK) {
+////////////////////////////////////////////////////////////////////////////////////////////////
+        //esp_err_t example_wifi_connect(void)
+        //{
+            ESP_LOGI(TAG, "Start example_connect.");
+            //example_wifi_start();
+////////////////////////////////////////////////////////////////////////////////////////////////
+            //void example_wifi_start(void)
+            //{
+                wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+                ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+                esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+                // Warning: the interface desc is used in tests to capture actual connection details (IP, gw, mask)
+                esp_netif_config.if_desc = EXAMPLE_NETIF_DESC_STA;
+                esp_netif_config.route_prio = 128;
+                s_example_sta_netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+                esp_wifi_set_default_wifi_sta_handlers();
+
+                ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+                ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+                ESP_ERROR_CHECK(esp_wifi_start());
+            //}// example_wifi_start ends
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+            wifi_config_t wifi_config = {
+                .sta = {
+        #if !CONFIG_EXAMPLE_WIFI_SSID_PWD_FROM_STDIN
+                    .ssid = CONFIG_EXAMPLE_WIFI_SSID,
+                    .password = CONFIG_EXAMPLE_WIFI_PASSWORD,
+        #endif
+                    .scan_method = EXAMPLE_WIFI_SCAN_METHOD,
+                    .sort_method = EXAMPLE_WIFI_CONNECT_AP_SORT_METHOD,
+                    .threshold.rssi = CONFIG_EXAMPLE_WIFI_SCAN_RSSI_THRESHOLD,
+                    .threshold.authmode = EXAMPLE_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+                },
+            };
+        
+            //return example_wifi_sta_do_connect(wifi_config, true);
+////////////////////////////////////////////////////////////////////////////////////////////////
+            //esp_err_t example_wifi_sta_do_connect(wifi_config_t wifi_config, bool wait)
+            //{
+                if (wait) {
+                    s_semph_get_ip_addrs = xSemaphoreCreateBinary();
+                    if (s_semph_get_ip_addrs == NULL) {
+                        return ESP_ERR_NO_MEM;
+                    }
+            #if CONFIG_EXAMPLE_CONNECT_IPV6
+                    s_semph_get_ip6_addrs = xSemaphoreCreateBinary();
+                    if (s_semph_get_ip6_addrs == NULL) {
+                        vSemaphoreDelete(s_semph_get_ip_addrs);
+                        return ESP_ERR_NO_MEM;
+                    }
+            #endif
+                }
+                s_retry_num = 0;
+                ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &example_handler_on_wifi_disconnect, NULL));
+                ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &example_handler_on_sta_got_ip, NULL));
+                ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &example_handler_on_wifi_connect, s_example_sta_netif));
+            #if CONFIG_EXAMPLE_CONNECT_IPV6
+                ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &example_handler_on_sta_got_ipv6, NULL));
+            #endif
+
+                ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+                ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+                esp_err_t ret = esp_wifi_connect();
+                if (ret != ESP_OK) {
+                    ESP_LOGE(TAG, "WiFi connect failed! ret:%x", ret);
+                    return ret;
+                }
+                if (wait) {
+                    ESP_LOGI(TAG, "Waiting for IP(s)");
+            #if CONFIG_EXAMPLE_CONNECT_IPV4
+                    xSemaphoreTake(s_semph_get_ip_addrs, portMAX_DELAY);
+            #endif
+            #if CONFIG_EXAMPLE_CONNECT_IPV6
+                    xSemaphoreTake(s_semph_get_ip6_addrs, portMAX_DELAY);
+            #endif
+                    if (s_retry_num > CONFIG_EXAMPLE_WIFI_CONN_MAX_RETRY) {
+                        return ESP_FAIL;
+                    }
+                }
+                return ESP_OK;
+            //}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+        //}// example_wifi_connect ends
+////////////////////////////////////////////////////////////////////////////////////////////////
+        return ESP_FAIL;
+    //}// if (example_wifi_connect() != ESP_OK)
+    ESP_ERROR_CHECK(esp_register_shutdown_handler(&example_wifi_shutdown));
+
     /*
 esp_err_t example_connect(void)
 {
