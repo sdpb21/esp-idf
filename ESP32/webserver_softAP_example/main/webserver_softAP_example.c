c46ba59fbe790d13a3a30fa17895c9d3837eefaa
sdpb21 <jytprf@gmail.com>
2025-11-15 11:48:44 -0300

    Not working code, it worked on the webserver_softAP_example2 project

--- ESP32/webserver_softAP_example/main/webserver_softAP_example.c
+++ ESP32/webserver_softAP_example/main/webserver_softAP_example.c
@@ -7,21 +7,22 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <string.h>
-//#include "freertos/FreeRTOS.h"
-//#include "freertos/task.h"
-#include <esp_system.h>
-#include <esp_wifi.h>
-#include <esp_event.h>
-#include <esp_log.h>
-#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
-#include <lwip/err.h>
-#include <lwip/sys.h>
+#include "lwip/err.h"
+#include "lwip/sys.h"
+////////////////////////////////////////////////////////////////////////////////////////////////
+#include "esp_system.h"
 
-#include <esp_err.h>
+#include "esp_err.h"
 #include <sys/param.h>
-//#include "nvs_flash.h"
-#include <esp_netif.h>
+#include "esp_netif.h"
 #include <esp_http_server.h>
 
 #include "driver/gpio.h"
@@ -245,19 +246,20 @@ static void configure_led (void)
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_EXAMPLE_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_EXAMPLE_WIFI_PASSWORD
-//#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
-//#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
-#define EXAMPLE_ESP_WIFI_CHANNEL   13
-#define EXAMPLE_MAX_STA_CONN       1
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
+#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+
+
 
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
