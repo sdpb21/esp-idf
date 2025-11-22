8d2557182f7fb1c06e15c10739aca652d3b5f61e
sdpb21 <jytprf@gmail.com>
2025-11-22 12:23:36 -0300

    Some comments changed with more correctly terms

--- ESP32/webserverRGB_example/main/main.c
+++ ESP32/webserverRGB_example/main/main.c
@@ -455,19 +455,22 @@ static esp_err_t rgb_example_wifi_sta_do_connect(wifi_config_t wifi_config, bool
     }
     // Clears the number of connection retries
     s_retry_num = 0;
-    /* Registers a WiFi event to the system event loop, when the station is disconnected, the 
-       function rgb_example_handler_on_wifi_disconnect (that is the handler registered) gets
+    /* Registers a WiFi event handler to the system event loop, when the station is disconnected,
+       the function rgb_example_handler_on_wifi_disconnect (that is the handler registered) gets
        called, later, check for errors */
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &rgb_example_handler_on_wifi_disconnect, NULL));
-    /* Registers an IP event to the system event loop, when the station got an IP, the handler
-       function rgb_example_handler_on_sta_got_ip gets called, after that, check for errors */
+    /* Registers an IP event handler to the system event loop, when the station got an IP, the
+       handler function rgb_example_handler_on_sta_got_ip gets called, after that, check for
+       errors */
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &rgb_example_handler_on_sta_got_ip, NULL));
-    /* Registers a WiFi event to the system event loop, when the station is connected, the handler
-       function rgb_example_handler_on_wifi_connect gets called, and then, check for errors */
+    /* Registers a WiFi event handler to the system event loop, when the station is connected, the
+       handler function rgb_example_handler_on_wifi_connect gets called, and then, check for
+       errors */
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &rgb_example_handler_on_wifi_connect, s_example_sta_netif));
 #if CONFIG_EXAMPLE_CONNECT_IPV6
-    /* Registers an IP event to the system event loop, when the station got an IPv6, the handler
-       function rgb_example_handler_on_sta_got_ipv6 gets called, after that, check for errors */
+    /* Registers an IP event handler to the system event loop, when the station got an IPv6, the 
+       handler function rgb_example_handler_on_sta_got_ipv6 gets called, after that, check for 
+       errors */
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &rgb_example_handler_on_sta_got_ipv6, NULL));
 #endif
 
